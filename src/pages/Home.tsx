9171111369fca4cc6cd970d9748120a9d006d752
avula-sneha03 <83127682+avula-sneha03@users.noreply.github.com>
2024-03-15 18:25:55 +0530

    Add files via upload

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+
+
+
+export function Home() {
+    return (
+        <>
+            <h1 className="fs-3rem align-item-center">
+                Welcom To My cart
+            </h1>
+            <a href="./store">
+                <button className="button-34" role="button">Go To store</button>
+            </a> </>
+
+
+    )
+}
